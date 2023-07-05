e0f00d743361a973fd173fc2e31cd7c8f03d2ed7
KsiRT <130671776+KsiRT@users.noreply.github.com>
2023-07-05 20:19:17 +0300

    first try

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,47 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
-console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems
+  .map((el) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${el.original}">
+        <img
+          class="gallery__image"
+          src="${el.preview}"
+          data-source="${el.original}"
+          alt="${el.description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryHTML);
+
+const onImageClick = (event) => {
+  event.preventDefault();
+  const {
+    target: { dataset },
+  } = event;
+  console.dir(event.target);
+  if (event.target.nodeName !== "IMG") {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src=${dataset.source} width="800" height="600">
+`);
+  instance.show();
+  const onEscape = (event) => {
+    console.dir("zakrili");
+    if (event.key === "Escape") {
+      instance.close(() => {
+        document.removeEventListener("keydown", onEscape);
+      });
+    }
+  };
+  document.addEventListener("keydown", onEscape);
+};
+
+gallery.addEventListener("click", onImageClick);
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,27 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+const gallery = document.querySelector(".gallery");
+
+const galleryHTML = galleryItems
+  .map((el) => {
+    return `
+    <li class="gallery__item">
+      <a class="gallery__link" href="${el.original}">
+        <img
+          class="gallery__image"
+          src="${el.preview}"
+          data-source="${el.original}"
+          alt="${el.description}"
+        />
+      </a>
+    </li>`;
+  })
+  .join("");
+gallery.insertAdjacentHTML("afterbegin", galleryHTML);
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: "alt",
+  captionDelay: 250,
+});
